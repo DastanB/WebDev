a180d673a4e594eb92849f0703bc0ebb0c573339
kundyzbekov <kundyzbekov98@gmail.com>
2019-03-01 19:08:02 +0600

    added faculty page

--- wingman-ng-master/src/app/app-routing.module.ts
+++ wingman-ng-master/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { IndexComponent } from './index/index.component';
 import { LogedinComponent } from './logedin/logedin.component';
 import { MyloginComponent } from './mylogin/mylogin.component';
 import { MyregisterComponent } from './myregister/myregister.component';
+import { FacultyComponent} from './faculty/faculty.component';
 
 const routes: Routes = [
   {path: '', component: IndexComponent },
   {path: 'welcome', component: LogedinComponent},
   {path: 'login', component: MyloginComponent},
-  {path: 'register', component: MyregisterComponent}
+  {path: 'register', component: MyregisterComponent},
+  {path: 'faculty', component: FacultyComponent}
 ];
 
 @NgModule({
